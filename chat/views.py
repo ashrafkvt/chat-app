2233d819d3ba3c0636d1dc6e2dab23f0cab3340f
ashrafkvt <ashrafkvt6197@gmail.com>
2023-09-23 00:56:03 +0530

    create initial urls and views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from chat.models import Room
+
+
+def index_view(request):
+    return render(request, 'index.html', {
+        'rooms': Room.objects.all(),
+    })
+
+
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(request, 'room.html', {
+        'room': chat_room,
+    })
