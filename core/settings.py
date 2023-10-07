0d29985b886c7779f8eec38e7ac19ffeda72a6ca
ashrafkvt <ashrafkvt6197@gmail.com>
2023-10-07 12:41:20 +0530

    config channnel layer

--- core/settings.py
+++ core/settings.py
@@ -78,6 +78,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
